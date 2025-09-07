6169e878318c576a47e0b0924520ccf683327eb1
CoDzBeast <zacharymc99@yahoo.com>
2025-09-06 23:08:04 -0700

    Add check status script and update package-lock

---
+++ check-status.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Checking deployment status of TSStudio website...
+echo.
+
+echo 1. Checking if the site is accessible...
+echo Please visit: https://codzbeast.github.io/TSStudio/
+echo.
+
+echo 2. If the site is not loading, try these steps:
+echo    a. Clear your browser cache and try again
+echo    b. Check the GitHub Actions tab for any errors
+echo    c. Verify that GitHub Pages is enabled in repository settings
+echo    d. Make sure the source is set to GitHub Actions
+echo.
+
+echo 3. Common issues and solutions:
+echo    - If you see a 404 error, wait a few more minutes for deployment to complete
+echo    - If assets are not loading, check that the base path is set correctly
+echo    - If the page looks broken, verify that all paths use the /TSStudio/ prefix
+echo.
+
+echo Your site should be available at: https://codzbeast.github.io/TSStudio/
+echo.
+pause
